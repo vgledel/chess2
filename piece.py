bd7388f8efd135efda2ccf3e655450731e6a8bd2
Vallaev <88078717+Vallaev@users.noreply.github.com>
2021-07-28 09:39:56 +0200

    Create piece.py

---
+++ piece.py
@@ -0,0 +1,13 @@
+class Piece:
+  
+  def __init__(self):
+    self.color = ""
+    self.type = ""
+    self.name = ""
+  
+  
+  def moves(self):
+    return []
+  
+  def can_be_taken(self,coord):
+    return true
